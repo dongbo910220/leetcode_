e62150b98881c26844fd8071dd58013e2fdfe8d4
dongbo910220 <1275604947@qq.com>
2021-04-11 16:41:06 +0800

    add

---
+++ 刷题leetcode/224. 基本计算器.py
@@ -0,0 +1,65 @@
+'''
+https://leetcode-cn.com/problems/basic-calculator/solution/ru-he-xiang-dao-yong-zhan-si-lu-lai-zi-y-gpca/
+'''
+
+
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res, num, sign = 0, 0, 1
+        stack = []
+        for c in s:
+            if c.isdigit():
+                num = num * 10 + int(c)
+            elif c in "+-":
+                res += sign * num
+                # update the num
+                num = 0
+                sign = 1 if c == "+" else -1
+            elif c == '(':
+                # using stack to keep previous status
+                stack.append(res)
+                stack.append(sign)
+                res = 0
+                sign = 1
+            elif c == ')':
+                res += sign * num
+                num = 0
+                sign = 1
+                res = stack.pop() * res
+                res += stack.pop()
+
+        # after the for loop:
+        res += sign * num
+        return res
+
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res, num, sign = 0, 0, 1
+        stack = []
+        for c in s:
+            if c.isdigit():
+                num = 10 * num + int(c)
+            elif c in "+-":
+                res += sign * num
+                num = 0
+                sign = 1 if c == '+' else -1
+            elif c == '(':
+                stack.append(res)
+                stack.append(sign)
+                res = 0
+                sign = 1
+            elif c == ')':
+                res += sign * num
+                num = 0
+                res *= stack.pop()
+                res += stack.pop()
+        res += sign * num
+        return res
---
+++ 刷题leetcode/76. 最小覆盖子串.py
@@ -0,0 +1,79 @@
+'''
+https://leetcode-cn.com/problems/minimum-window-substring/solution/tong-su-qie-xiang-xi-de-miao-shu-hua-dong-chuang-k/
+'''
+
+from collections import Counter
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        need = Counter(t)
+        needCnt = len(t) #record the amount of key
+        i = 0
+        res = (0, float('inf'))
+        for j, c in enumerate(s):
+            #slide right
+            if need[c] > 0:
+                needCnt -= 1
+            need[c] -= 1
+            if needCnt == 0:
+                while True:
+                    #slide left
+                    c_head = s[i]
+                    if need[c_head] == 0:
+                        break
+                    need[c_head] += 1
+                    i += 1
+                # update
+                if j - i < res[1] - res[0]:
+                    res = (i, j)
+                need[c_head] += 1
+                i += 1
+                needCnt += 1
+        return '' if res[1] > len(s) else s[res[0]:res[1]+1]
+
+from collections import Counter
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        need = Counter()
+        # check if counter default value is 0
+        for char in t:
+            need[char] += 1
+        print(need)
+        needCnt = len(t)
+        i = 0
+        res = (0, float('inf'))  # record the head idx and tail idx
+        for j, c in enumerate(s):
+            if need[c] > 0:
+                needCnt -= 1
+            need[c] -= 1
+            if needCnt == 0:
+                while True:
+                    c_head = s[i]
+                    if need[c_head] == 0:
+                        break
+                    need[c_head] += 1
+                    i += 1
+                if j - i < res[1] - res[0]:
+                    res = (i, j)
+                need[c_head] += 1
+                needCnt += 1
+                i += 1
+        return '' if res[1] > len(s) else s[res[0]:res[1] + 1]
+
+
+
+
+
+
+
+
+
